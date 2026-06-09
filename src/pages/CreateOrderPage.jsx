862d46480577336700e472fff515807ecc169545
AuraK-7 <23301132@lilingkun.com>
2026-06-10 01:10:48 +0800

    fix: lint配置兼容 + 修复编码错误
    Change-Id: Id879bcc6719466bea69407898755abc91c744396

--- src/pages/CreateOrderPage.jsx
+++ src/pages/CreateOrderPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router';
 
 import { ServiceContext } from '../contexts/ServiceContext';
@@ -10,27 +10,26 @@ const CreateOrderPage = () => {
   const navigate = useNavigate();
 
   const onSubmitClick = () => {
-    // 1. 鍒涘缓璁㈠崟锛屾嬁鍒皁rderid
+    // 1. 创建订单，拿到orderId
     const parsedGoodId = parseInt(goodId, 10);
     const good = services.good.getGoodById(parsedGoodId);
     if (!good) {
-      //TBD 璺宠浆涓婚〉
-      alert('鍟嗗搧涓嶅瓨鍦?);
+      // TBD 跳转主页
+      alert('商品不存在');
       navigate('/home');
       return;
     }
     const order = services.order.createOrder(1, parsedGoodId, good.price)
-    // 2. 璺宠浆鍒版敮浠橀〉闈?
-    alert('涓嬪崟鎴愬姛锛岃鏀粯锛?);
+    // 2. 跳转到支付页面
+    alert('下单成功，请支付！');
     navigate(`/pay/${order.id}`);
-    
   }
 
   return <>
     <h1>CreateOrder Page</h1>
-    <p> goodId: {goodId}</p>
-    <p>浣犳槸鍚﹁璐拱锛燂紵锛燂紵锛燂紵</p>
-    <button onClick={onSubmitClick}>璐拱</button>
+    <p>goodId: {goodId}</p>
+    <p>是否要购买？</p>
+    <button onClick={onSubmitClick}>购买</button>
   </>
 }
 
